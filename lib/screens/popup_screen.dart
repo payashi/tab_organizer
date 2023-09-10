cd63be59f78e812de95184cdca71a4064b0b3ce3
payashi <6phyphy6@gmail.com>
2023-09-10 11:19:05 +0900

    Fix api bug

--- lib/screens/popup_screen.dart
+++ lib/screens/popup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tab_organizer/chrome_api.dart';
-import 'package:tab_organizer/models/category_info.dart';
 import 'package:tab_organizer/models/chrome_tab.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
